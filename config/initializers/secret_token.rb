9a31b211deb3573fba6266c4a9fb035afdc336e1
Leet Rocket <leetrocket@Leet-Rockets-MacBook-Pro.local>
2011-12-07 01:22:50 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Writedown::Application.config.secret_token = 'dd3c95afbf4def8930c5d39d7546ba96f3b20f4ed5af6702792f7bb48d928949ae6f22edb942a997f3343d3a633bd10a79c91099ff226aff5d5b1ecb3b466fb9'
